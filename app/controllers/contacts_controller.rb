dec5ed215c3508f146e9d4d2a65e3aaee787de62
Glenn Hanna <glennhanna@yahoo.com>
2015-07-21 07:15:43 +0000

    Added contact submit functionality

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,8 +1,24 @@
 class ContactsController < ApplicationController
     def new
-        @contact = Contact.new
+      @contact = Contact.new
     end
     
     def create
+      @contact = Contact.new(contact_params)
+      
+      # save contact data into database
+      if @contact.save
+        flash[:success] = 'Message sent.'
+        redirect_to new_contact_path
+      else
+        flash[:danger] = 'Error occured, message has not been sent.'
+        redirect_to new_contact_path
+      end
     end
+    
+    private
+    # white list for security. Very important
+      def contact_params
+        params.require(:contact).permit(:name, :email, :comments)  
+      end
 end
